fa9acf0e56d985abf270b963cf0956fd636144e7
daisukekoga8gif <daisuke.koga.8@gmail.com>
2026-05-22 18:33:36 +0900

    remove vercel banner

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '2mb',
-    },
-  },
-  // 外部画像ドメインは使用しない方針
   images: {
     unoptimized: true,
   },
